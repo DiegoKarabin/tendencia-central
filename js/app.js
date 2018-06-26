483803cab387d86a842e14f5eee218034deee383
Diego Karabin <diegokarabin@gmail.com>
2018-06-25 20:13:01 -0430

    Validacion solo numeros en el formulario

--- js/app.js
+++ js/app.js
@@ -5,33 +5,52 @@ Vue.component('app-single-value-form', {
             valor: undefined,
         }
     },
+    computed: {
+        habilitar() {
+            return this.valor && !isNaN(Number(this.valor));
+        }
+    },
     methods: {
         enviar() {
-            this.$emit('enviar', this.valor);
+            this.$emit('enviar', Number(this.valor));
             this.valor = undefined;
         }
     }
 });
+
+Vue.component('app-btn', {
+    template: '<button :class="css_classes" @click="action">{{ text }}</button>',
+    props: ['type', 'action', 'text'],
+    computed: {
+        css_classes() {
+            return 'btn btn-' + this.type;
+        }
+    }
+});
+
+Vue.component('app-dataset', {
+    template: '#dataset',
+    props: ['conjunto'],
+    methods: {
+        retirar(indice) {
+            this.$emit('remove', indice);
+        }
+    }
+})
 
 let vm = new Vue({
     el: '#app',
     data: {
         conjunto: [],
         mostrar: false,
-    },
-    computed: {
-        media_aritmetica() {
-            if (this.conjunto.length)
-                return this.conjunto.reduce(
-                    (xi, acum) => acum + xi
-                ) / this.conjunto.length;
-        }
+        media_aritmetica: undefined,
+        mediana: undefined,
+        moda: undefined
     },
     methods: {
         agregar(valor) {
-            if (this.conjunto.length < 30) {
-                this.conjunto.push(Number(valor));
-            }
+            if (this.conjunto.length < 30)
+                this.conjunto.push(valor);
         },
 
         retirar(indice) {
@@ -45,7 +64,15 @@ let vm = new Vue({
 
         calcular() {
             this.conjunto.sort((a, b) => a - b);
+            this.calcular_media_aritmetica();
             this.mostrar = true;
+        },
+
+        calcular_media_aritmetica() {
+            if (this.conjunto.length)
+                this.media_aritmetica = this.conjunto.reduce(
+                    (xi, acum) => acum + xi
+                ) / this.conjunto.length;
         }
     }
 })
